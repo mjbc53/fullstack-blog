58b441f61f206543f6283c72ce554ed5a8d84a30
Mark <mjbc53@gmail.com>
2021-11-21 20:39:55 -0600

    added include statment to one of User model routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -19,13 +19,17 @@ router.get('/:id', (req, res) => {
     attributes: { exclude: ['password']},
     where: {
       id: req.params.id
-    } //,
-    // include: [
-    //   {
-    //     model: Post,
-    //     attributes: [ 'id', 'title', 'post_content', 'created_at' ]
-    //   }
-    // ]
+    },
+    include: [
+      {
+        model: Post,
+        attributes: [ 'id', 'title', 'post_content', 'created_at' ]
+      },
+      {
+        model: Comment,
+        attributes: [ 'id', 'comment_content', 'post_id']
+      }
+    ]
   })
   .then( dbUserData => {
     if(!dbUserData){
